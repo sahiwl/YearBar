de38e12b36ee5cba3fb3c80f117bdb71560da5d1
sahiwl <piyushkrray1@gmail.com>
2026-02-17 20:02:11 +0530

    first commit

---
+++ YearBar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TimeLeft
+//
+//  Created by Sahil Kumar Ray on 17/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YearBar/YearBar.swift
@@ -0,0 +1,17 @@
+//
+//  YearBarApp.swift
+//  Yearbar
+//
+//  Created by Sahil Kumar Ray on 17/02/26.
+//
+
+import SwiftUI
+
+@main
+struct YearbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
